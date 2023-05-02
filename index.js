90c9473a29c2b92c9a3b1099cfd8bc31ecf78b7c
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-05-02 11:49:26 -0700

    added REST API

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 "use stict";
+import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
 import { Car } from "./models/Car.js";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 app.set("port", process.env.PORT || 3000);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.get("/api/cars", async (req, res) => {
+  try {
+    const cars = await Car.find({});
+    res.json(cars);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+app.get("/api/cars/:manufacturer", async (req, res) => {
+  try {
+    const car = await Car.findOne({ manufacturer: req.params.manufacturer });
+    if (!car) {
+      res.sendStatus(404);
+    } else {
+      res.json(car);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+app.post("/api/cars", async (req, res) => {
+  try {
+    const car = new Car(req.body);
+    await car.save();
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+app.delete("/api/cars/:manufacturer", async (req, res) => {
+  try {
+    const result = await Car.deleteOne({
+      manufacturer: req.params.manufacturer,
+    });
+    if (result.deletedCount === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
 
 app.get("/", async (req, res) => {
   try {
@@ -24,10 +77,7 @@ app.get("/about", (req, res) => {
   res.sendFile("/public/about.html");
 });
 app.get("/details/:manufacturer", async (req, res) => {
-  // const manufacturer = parseInt(req.query.manufacturer);
-  // const { manufacturer } = req.params;
   try {
-    // const car = await Car.findOne({ manufacturer });
     const car = await Car.findOne({ manufacturer: req.params.manufacturer });
     if (!car) {
       res.sendStatus(404);
